d1d2a6cc712ed94cad233e1e94f6c45c3b429240
Greg Nofi <crackityjones2004-g@yahoo.com>
2012-09-02 19:42:20 -0400

    Add more handling for response content. Use JSONP.

--- server/requestHandlers.js
+++ server/requestHandlers.js
@@ -4,15 +4,30 @@ var querystring = require("querystring");
 function lookup(query, response) {
   console.log("Look up birthday in DB");
 
+  var callback = querystring.parse(query)["callback"];
+
   var year = querystring.parse(query)["year"];
   var month = querystring.parse(query)["month"];
   var day = querystring.parse(query)["day"];
   var key = year + month + day;
-  var content = birthdayDB[key];
 
-  response.writeHead(200, {"Content-Type": "text/html"});
-  response.write(content);
-  response.end();
+  console.log(query.callback);
+  try {
+    var content = birthdayDB[key];
+  } catch (error) {
+    response.writeHead(400, {"Content-Type": "text/plain"});
+    response.write("Error");
+    response.end();
+  }
+
+  if (typeof content === "undefined") {
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.write("No Birthday Found");
+    response.end();
+  }
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  console.log(callback + "('" + content + "')");
+  response.end(callback + "('" + content + "')");
 }
 
 function upload(response) {
